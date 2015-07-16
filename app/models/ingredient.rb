5855a7259953729da4521052526612e8c2395f8c
libbyschuknight <libbysk@gmail.com>
2015-07-16 15:35:02 +1200

    Correct Ingredient migration.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ActiveRecord::Base
-  has_many: mealsIngredients
-  has_many: meals, through: :mealsIngredients
+  has_many :mealsIngredients
+  has_many :meals, through: :mealsIngredients
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Ingredient.destroy_all
+
+
+Ingredient.create(name: "spring onions")
+Ingredient.create(name: "onions")
+Ingredient.create(name: "beef fillet")
+Ingredient.create(name: "soy sauce")
+Ingredient.create(name: "jasmine rice")
+Ingredient.create(name: "pork fillet")
+Ingredient.create(name: "kumera")
+
