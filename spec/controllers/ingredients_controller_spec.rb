7ec73c00de462a4e3a8128ef7cf6d4572bee2138
libbyschuknight <libbysk@gmail.com>
2015-07-17 15:10:09 +1200

    Trying to add testing for JSON rendering. Have used Airborne gem, I am getting something returned in the test but it is not passing. Something to do with an property of an object. Not sure what is happening. Will come back to this.

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
+require 'airborne' # for testing JSON output when will be API
+
 
 RSpec.describe IngredientsController, type: :controller do
   before do
     Ingredient.destroy_all
   end
 
-  xdescribe "#index" do
+  # controller test before rendering output as JSON
+  describe "#index" do
     before do
       @ingredients = FactoryGirl.create_list(:ingredient, 5)
     end
@@ -15,6 +18,47 @@ RSpec.describe IngredientsController, type: :controller do
       expect(assigns(:ingredients)).to eq(@ingredients)
     end
   end
+
+
+
+
+
+  ###### Trying to do JSON testing - haven't got working yet :(  ####
+  # this is likely to be in the right place as airborne gem works with rspec-rails
+  # describe "GET index" do
+  #   before do
+  #     # @ingredients = FactoryGirl.create_list(:ingredient, 5)
+  #     @ingredients = []
+  #     @ingredients << Ingredient.create(name: "spring onions")
+  #   end
+
+  #   it "should validate types" do
+  #     get :index, :format => 'json' #if your route responds to both html and json
+  #     expect_json_types({id: :int, name: :string})
+  #   end
+
+    # it "should validate values" do
+    #   get :index
+    #   expect_json({:name => "spring onions"})
+    # end
+  # end
+
+
+  # wanting to test rendering JSON
+    # using gem 'Airborne' - https://github.com/brooklynDev/airborne/blob/master/README.md
+    # not sure if this is the right place or if should be in a feature spec file?
+
+  # describe '#index JSON rendering' do
+  #   it 'should validate types' do
+  #     get 'http://localhost:3000/ingredients' #json api that returns { "name" : "spring onions" }
+  #     expect_json_types({name: :string})
+  #   end
+
+  #   # it 'should validate values' do
+  #   #   get 'http://example.com/api/v1/simple_get' #json api that returns { "name" : "John Doe" }
+  #   #   expect_json({:name => "John Doe"})
+  #   # end
+  # end
 
 
 end
