85564e04bcbf46b08183ffa02504fa699151b5f1
libbyschuknight <libbysk@gmail.com>
2015-07-22 12:17:59 +1200

    Add migration for meal model.

--- db/migrate/20150717033107_create_meals.rb
+++ db/migrate/20150717033107_create_meals.rb
@@ -1,6 +1,7 @@
 class CreateMeals < ActiveRecord::Migration
   def change
     create_table :meals do |t|
+      t.belongs_to :mealPlan, index: true
       t.string :name
       t.string :imageURL
       t.text :cooking_instructions
