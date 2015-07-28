3acd06bf77d5569908d53e28d36472b29b0799b5
libbyschuknight <libbysk@gmail.com>
2015-07-28 15:06:26 +1200

    Add test for ingredients on meal#show page. Also have worked out how to get information on meal#show page for ingredients and meal_ingredients.

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,6 +6,14 @@ class MealsController < ApplicationController
 
   def show
     @meal = Meal.find_by_id(params[:id])
+
+    # add in here some sort of method that gets the
+    # ingredients details - name
+    # and the meal_ingreidents detials - quantity and measurement
+    # stores in something? a hash?
+    # and then can use in view really easily!!!
+
+
   end
 
 end
--- spec/features/user_can_view_meal_spec.rb
+++ spec/features/user_can_view_meal_spec.rb
@@ -6,6 +6,7 @@ require 'rails_helper'
 
 feature "User can view a meal" do
   let(:meal) { FactoryGirl.create(:meal, name: "Five Spice Pork", imageURL: "http://www.taste.com.au/images/recipes/agt/2006/12/14680_l.jpg")}
+  # let(:) { FactoryGirl.create(:meal, name: "Five Spice Pork", imageURL: "http://www.taste.com.au/images/recipes/agt/2006/12/14680_l.jpg")} ????
 
   background { visit meal_path(meal) }
 
@@ -21,6 +22,11 @@ feature "User can view a meal" do
     expect(page).to have_xpath("//image[@src = 'http://www.taste.com.au/images/recipes/agt/2006/12/14680_l.jpg']")
   end
 
+
+  scenario "it shows the ingredient details" do
+    expect(page).to have_content("spring onions")
+
+  end
 
 end
 
