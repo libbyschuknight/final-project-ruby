9896fd4c8ca00527a5efb3da7316a514c7d54828
libbyschuknight <libbysk@gmail.com>
2015-07-29 19:49:12 +1200

    Add description to view spec for meal.

--- spec/features/user_can_view_meal_spec.rb
+++ spec/features/user_can_view_meal_spec.rb
@@ -16,6 +16,7 @@ feature "User can view a meal" do
     # puts page.body
     expect(page).to have_content(meal.name)
     expect(page).to have_content("Five Spice Pork")
+    expect(page).to have_content("Heat half the oil in a wok over high heat. Add one-third of the pork and stir-fry for 5 minutes or until brown all over. Transfer to a plate. Repeat, in 2 more batches, with the remaining pork. Step 2 Heat remaining oil in wok over medium heat. Add eschalot and garlic and cook for 5 minutes or until golden. Add pork, soy sauce, palm sugar, water, star anise and five spice. Bring to the boil. Reduce heat to low and cook, covered, stirring occasionally, for 1 1/2 hours or until pork is tender. Increase heat to high and bring to the boil. Cook for 10 minutes or until sauce thickens. Step 3 Add the fish sauce and half the shallot and stir to combine. Place in a serving bowl. Top with remaining shallot. Serve with steamed rice topped with cucumber and coriander leaves.")
   end
 
   scenario "it shows a meals' image" do
