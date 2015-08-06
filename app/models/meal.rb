84889c4b4faa60e26ce4bb9ffe04e62b7b0dc6a7
libbyschuknight <libbysk@gmail.com>
2015-08-06 15:55:03 +1200

    Add comments for Samson re meal model and getting ingredient information.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,88 +4,78 @@ class Meal < ActiveRecord::Base
   belongs_to :mealPlan
 
 
+  ### want to store the ingrdients name, quantity and  measurement
+  ### have been trying two ways
+
+  ### 1 - storing information in nested hash
+  ### I've tried it in a couple of different ways and am getting nested hash but not quite the right info
+
   def ingredient_details
+    details = {}
 
-    # Meal.joins(:meal_ingredients)
-
-    meal = Meal.joins(:meal_ingredients, :ingredients)
-
-
-    # meal = MealIngredient.find
-    # details = MealIngredient.joins(:meal, :ingredient)
-
-    meal.each do |item|
-      item.ingredients
+    ingredients.each do |ingredient|
+      details["#{ingredient.name}"] = {}
+      details["#{ingredient.name}"]["name"] = ingredient.name
     end
 
+    meal_ingredients.each do |ing|
+      details.map do |key, value|
+        details[key]["quantity"] = ing.quantity
+        details[key]["measurement"] = ing.measurement
+      end
+    end
+
+
+    # ingredients.each do |ingredient|
+    #   details["#{ingredient.id}"] = {}
+    #   details["#{ingredient.id}"]["name"] = ingredient.name
+    # end
+
+    # details.each do |key, value|
+    #   details[key]["quantity"] = MealIngredient.find(key).quantity
+    #   details[key]["measurement"] = MealIngredient.find(key).measurement
+    # end
+
+    details
   end
 
+  ## the output I am getting - for first, second one keys for first hash have just replcaed with the ingredient ID
+
+  # {"spring onions"=>{
+  #     "name"=>"spring onions",
+  #     "quantity"=>1.0,
+  #     "measurement"=>"cup"},
+  #   "beef fillet"=>{
+  #     "name"=>"beef fillet",
+  #     "quantity"=>1.0,
+  #     "measurement"=>"cup"},
+  #   "soy sauce"=>{
+  #     "name"=>"soy sauce",
+  #     "quantity"=>1.0,
+  #     "measurement"=>"cup"},
+  #     "jasmine rice"=>{
+  #       "name"=>"jasmine rice",
+  #       "quantity"=>1.0,
+  #       "measurement"=>"cup"}
+  #     }
 
 
-  # refactor - use join table in ActiveRecord to get same info
+
+
+  ### 2 - as I was struggling with the hash idea, started looking into the activerecord query but don't really know what doing with it
   # def ingredient_details
-  #   details = {}
 
-  #   ingredients.each do |ingredient|
-  #     details["#{ingredient.id}"] = {}
-  #     details["#{ingredient.id}"]["name"] = ingredient.name
+  #   # Meal.joins(:meal_ingredients)
+
+  #   meal = Meal.joins(:meal_ingredients, :ingredients)
+
+  #   # meal = MealIngredient.find
+  #   # details = MealIngredient.joins(:meal, :ingredient)
+
+  #   meal.each do |item|
+  #     item.ingredients
   #   end
-
-  #   details.each do |key, value|
-  #     details[key]["quantity"] = MealIngredient.find(key).quantity
-  #     details[key]["measurement"] = MealIngredient.find(key).measurement
-  #   end
-
-
-
-  #   # ingredients.each do |ingredient|
-  #   #   details["#{ingredient.name}"] = {}
-  #   #   details["#{ingredient.name}"]["name"] = ingredient.name
-  #   # end
-
-
-  #   # meal_ingredients.each do |ing|
-  #   #   details.map do |key, value|
-  #   #     details[key]["quantity"] = ing.quantity
-  #   #     details[key]["measurement"] = ing.measurement
-  #   #   end
-  #   # end
-
-
-
-
-
-  #   # meal_ingredients.each do |ing|
-
-  #   #   details["#{ingredient.name}"]["quantity"] = ing.quantity
-  #   #   details["#{ingredient.name}"]["measurement"] = ing.measurement
-  #   #   # value["measurement"] =ing.measurement
-
-
-  #   # end
-
-  #   # meal_ingredients.each do |ing|
-  #   #   details.each do |key, value|
-  #   #     key["quantity"] = ing.quantity
-  #   #   end
-  #   # end
-
-
-  #   details
-
   # end
 
+
 end
-
-
-# ingredients.each do |ingredient|
-#   # ing_name = ingredient.name
-#   details["#{ingredient.name}"] = {}
-
-# end
-
-
-# meal_ingredients.each do |ingredient|
-#   details["#{ing_name}"] ["quantity"] = ingredient.quantity
-#   details["#{ing.name}"] ["measurement"] = ingredient.measurement
-# end
