413785c91e238d896ffdec66147fec76c5f139ca
Samson Ootoovak <sam@enspiral.com>
2015-08-07 12:11:03 +1200

    Using ActiveRecord associations.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,81 +1,24 @@
 class Meal < ActiveRecord::Base
+
   has_many :meal_ingredients
   has_many :ingredients, through: :meal_ingredients
   belongs_to :mealPlan
 
-
-  ### want to store the ingrdients name, quantity and  measurement
-  ### have been trying two ways
-
-  ### 1 - storing information in nested hash
-  ### I've tried it in a couple of different ways and am getting nested hash but not quite the right info
-
   def ingredient_details
-    details = {}
-
-    ingredients.each do |ingredient|
-      details["#{ingredient.name}"] = {}
-      details["#{ingredient.name}"]["name"] = ingredient.name
+    details = meal_ingredients.includes(:ingredient)
+    details.each_with_object({}) do |detail, result|
+      name = detail.ingredient.name
+      result[name] = detail_object(name, detail)
     end
-
-    meal_ingredients.each do |ing|
-      details.map do |key, value|
-        details[key]["quantity"] = ing.quantity
-        details[key]["measurement"] = ing.measurement
-      end
-    end
-
-
-    # ingredients.each do |ingredient|
-    #   details["#{ingredient.id}"] = {}
-    #   details["#{ingredient.id}"]["name"] = ingredient.name
-    # end
-
-    # details.each do |key, value|
-    #   details[key]["quantity"] = MealIngredient.find(key).quantity
-    #   details[key]["measurement"] = MealIngredient.find(key).measurement
-    # end
-
-    details
   end
 
-  ## the output I am getting - for first, second one keys for first hash have just replcaed with the ingredient ID
+private
 
-  # {"spring onions"=>{
-  #     "name"=>"spring onions",
-  #     "quantity"=>1.0,
-  #     "measurement"=>"cup"},
-  #   "beef fillet"=>{
-  #     "name"=>"beef fillet",
-  #     "quantity"=>1.0,
-  #     "measurement"=>"cup"},
-  #   "soy sauce"=>{
-  #     "name"=>"soy sauce",
-  #     "quantity"=>1.0,
-  #     "measurement"=>"cup"},
-  #     "jasmine rice"=>{
-  #       "name"=>"jasmine rice",
-  #       "quantity"=>1.0,
-  #       "measurement"=>"cup"}
-  #     }
-
-
-
-
-  ### 2 - as I was struggling with the hash idea, started looking into the activerecord query but don't really know what doing with it
-  # def ingredient_details
-
-  #   # Meal.joins(:meal_ingredients)
-
-  #   meal = Meal.joins(:meal_ingredients, :ingredients)
-
-  #   # meal = MealIngredient.find
-  #   # details = MealIngredient.joins(:meal, :ingredient)
-
-  #   meal.each do |item|
-  #     item.ingredients
-  #   end
-  # end
-
+  def detail_object(name, detail)
+    name        = detail.ingredient.name
+    quantity    = detail.quantity
+    measurement = detail.measurement
+    { "name" => name, "quantity" => quantity, "measurement" => measurement }
+  end
 
 end
