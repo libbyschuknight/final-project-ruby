c6cbb41fc94bbb87c4a36b0456418e91468bc099
libbyschuknight <libbysk@gmail.com>
2015-09-23 09:26:33 +1200

    Update create method with ternary operator

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -20,12 +20,7 @@ class MealsController < ApplicationController
 
   def create
     @meal = Meal.new(meal_params)
-    # @meal.save ? redirect_to(@meal) : redirect_to(new_meal_path)
-    if @meal.save
-      redirect_to(@meal)
-    else
-      redirect_to(new_meal_path)
-    end
+    @meal.save ? redirect_to(@meal) : redirect_to(new_meal_path)
   end
 
   private
