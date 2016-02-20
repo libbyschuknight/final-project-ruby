96462eb6543ffe016ad85983db3080bb34325a7a
libbyschuknight <libbysk@gmail.com>
2016-02-20 15:37:05 +1300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922213542) do
+ActiveRecord::Schema.define(version: 20151025072042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 20150922213542) do
   end
 
   add_index "meals", ["meal_plan_id"], name: "index_meals_on_meal_plan_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "meal_ingredients", "ingredients"
   add_foreign_key "meal_ingredients", "meals"
